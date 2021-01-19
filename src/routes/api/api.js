c86896ced529602c7b3dcac997c4910da73327c1
Matias Gonzalo Vivas Castillo <mativivas77@gmail.com>
2021-01-19 15:15:57 -0300

    Revert "Agrego funcionalidades de API, para la conexión con React"
    This reverts commit e597ef56188f8b3bee50023507d3f775c659d315.

--- src/routes/api/api.js
+++ src/routes/api/api.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const courseController = require('../../controllers/api/courseController')
-const userController = require('../../controllers/api/userController')
 
 router.get('/course', courseController.list);
-
-
-router.get('/user/list', userController.list);
-router.get('/user/search/:id', userController.searchById);
 
 module.exports = router;
